925d0b0b100c6de0d00389ada14f880173185078
Yağız Yaşar <yagizyasarr9@gmail.com>
2021-10-20 18:50:25 +0300

    add vibranium material

--- src/MaterialManager.py
+++ src/MaterialManager.py
@@ -19,10 +19,15 @@ class MaterialManager:
                 self.carManager.setMaterial(materialType.Carbon)
                 returnText = "Material is changed to Carbon."
 
+            case materialType.Vibranium:
+                self.carManager.setMaterial(materialType.Vibranium)
+                returnText = "Material is changed to Vibranium."
+
             case _:
-                returnText = "Material seletion is invalid."
+                returnText = "Material selection is invalid."
 
         return self._announceMaterial(returnText)
+
 
     def _announceMaterial(self, text: str) -> bool:
         return self.announcer(text)
